cc73860f1186ea1ef21ee27c26106ae2b3fcf4f3
Sayak Halder <sayak.halder@accenture.com>
2024-12-08 14:06:57 +0530

    Disable SpringTestApp for UserRepositoryImpl

--- src/test/java/com/sayak/journalApp/repository/UserRepositoryImplTest.java
+++ src/test/java/com/sayak/journalApp/repository/UserRepositoryImplTest.java
@@ -2,6 +2,7 @@ package com.sayak.journalApp.repository;
 
 import com.sayak.journalApp.entity.Users;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
+@Disabled
 public class UserRepositoryImplTest {
     @Autowired
     private UserRepositoryImpl userRepository;
     @Test
+    @Disabled
     public void getUserForSATest(){
         Assertions.assertNotNull(userRepository.getUserForSA());
     }
